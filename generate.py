fdc0365f0b6ddfdcbbf15698f336fe6e5abf3934
Zak Nelson <zak.nelson@gmail.com>
2016-07-07 13:03:36 -0700

    Support for Amazon lambda.

--- generate.py
+++ generate.py
@@ -1,16 +1,22 @@
-import markovify, nltk, sys
+import markovify, sys
 
-with open("description.txt") as f:
+def generate(numSentences):
+  with open("description.txt") as f:
     text = f.read()
+  textModel = markovify.Text(text)
+  bestSentences = []
+  while (True):
+    sentence = textModel.make_short_sentence(140).capitalize()
+    wordCount = len(sentence.split(" "))
+    if (sentence.find(" is a ") != -1 and wordCount > 5):
+      bestSentences.append(sentence)
+      if (len(bestSentences) == numSentences):
+        break;
 
-textModel = markovify.Text(text)
+  return bestSentences
 
-bestSentence = None
-while (True):
-  sentence = textModel.make_sentence().capitalize()
-  wordCount = len(sentence.split(" "))
-  if (sentence.find(" is a ") != -1 and wordCount > 5):
-    bestSentence = sentence
-    break;
+def amazon_lambda_handler(event, context):
+  return generate(min(100, event.count))
 
-print bestSentence
+if __name__ == "__main__":
+  print generate(1)[0]
