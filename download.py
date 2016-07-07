f871a606c6e16d6a4f7684e57210d2789aa948e7
Max Nelson <maxnelso@gmail.com>
2016-07-07 13:56:53 -0700

    Cleanups and title fix

--- download.py
+++ download.py
@@ -1,66 +1,68 @@
 import json, nltk.data, re, time, urllib
 
 CORPUS_SIZE = 500
-SENTENCE_DETECTOR = nltk.data.load("tokenizers/punkt/english.pickle")
+SENTENCE_DETECTOR = nltk.data.load('tokenizers/punkt/english.pickle')
 
-def cleanString(str):
-  str = re.sub(r"http\S+", " ", str)
-  str = re.sub(r"<.*?>", " ", str)
-  str = re.sub(r"&.*?;", " ", str)
-  str = str.encode("ascii", "ignore")
-  return str
+def clean_string(s):
+  s = re.sub(r'http\S+', ' ', s)
+  s = re.sub(r'<.*?>', ' ', s)
+  s = re.sub(r'&.*?;', ' ', s)
+  s = s.encode('ascii', 'ignore')
+  return s
 
-def getAppData(appID):
-  url = "http://store.steampowered.com/api/appdetails?appids=" + str(appID)
+def get_app_data(app_id):
+  url = 'http://store.steampowered.com/api/appdetails?appids=' + str(app_id)
   response = urllib.urlopen(url)
   try:
     data = json.loads(response.read())
-    if (not data[str(appID)]["success"] or data[str(appID)]["data"]["type"] != "game"):
+    if not data[str(app_id)]['success'] or data[str(app_id)]['data']['type'] != 'game':
       return None
-    return data[str(appID)]
+    return data[str(app_id)]
   except:
     return None
 
-def getApps():
-  url = "http://api.steampowered.com/ISteamApps/GetAppList/v2/"
+def get_apps():
+  url = 'http://api.steampowered.com/ISteamApps/GetAppList/v2/'
   response = urllib.urlopen(url)
   try:
     data = json.loads(response.read())
-    apps = data["applist"]["apps"]
+    apps = data['applist']['apps']
     return apps
   except:
     return None
 
-def getDescriptionFromAppData(appData):
-  description = cleanString(appData["data"]["detailed_description"])
+def get_description_from_app_data(app_data):
+  description = clean_string(app_data['data']['detailed_description'])
   sentences = SENTENCE_DETECTOR.tokenize(description.strip())
-  if (len(sentences) > 0):
+  if len(sentences) > 0:
     sentences = sentences[0:(min(3, len(sentences)))]
-    sentences = [x for x in sentences if len(x.split(" ")) > 5 and not x.split(" ")[0].isupper() and x.find("\r") == -1]
-    combinedSentence = " ".join(sentences)
-    if (len(combinedSentence) == 0 or not combinedSentence[0].isalpha() or len(combinedSentence.split(" ")) < 5):
+    sentences = [x for x in sentences if len(x.split(' ')) > 5 and not x.split(' ')[0].isupper() and x.find('\r') == -1]
+    combined_sentence = ' '.join(sentences)
+    if len(combined_sentence) == 0 or not combined_sentence[0].isalpha() or len(combined_sentence.split(' ')) < 5:
       return None
-    return combinedSentence
+    return combined_sentence
   return None
 
-def getTitleFromAppData(appData):
-  return cleanString(appData["data"]["name"])
+def get_title_from_app_data(app_data):
+  return clean_string(app_data['data']['name'])
 
 def main():
-  apps = getApps()[-CORPUS_SIZE:]
+  apps = get_apps()[-CORPUS_SIZE:]
 
   for app in apps:
-    appID = app["appid"]
-    appData = getAppData(appID)
-    if (not appData):
+    app_id = app['appid']
+    app_data = get_app_data(app_id)
+    if not app_data:
       continue
-    with open("description.txt", "a") as descriptionFile:
-      description = getDescriptionFromAppData(appData)
+    with open('description.txt', 'a') as description_file:
+      description = get_description_from_app_data(app_data)
       if (description):
-        descriptionFile.write(description + " ")
-    with open("title.txt", "a") as titleFile:
-      title = getTitleFromAppData(appData)
+        description_file.write(description + ' ')
+    with open('title.txt', 'a') as title_file:
+      title = get_title_from_app_data(app_data)
       if (title):
-        titleFile.write(title + " ")
+        title_file.write(title + ' ')
     time.sleep(2)
-main()
+
+if __name__ == "__main__":
+  main()
--- generate.py
+++ generate.py
@@ -1,21 +1,43 @@
 import markovify, sys
 
-def generate(numSentences):
-  with open("description.txt") as f:
+def cleanup_sentence(sentence):
+  is_a_index = sentence.find(' is a ')
+  sentence = sentence[:is_a_index].title() + sentence[is_a_index:]
+  return sentence
+
+def is_good_sentence(sentence):
+  wordCount = len(sentence.split())
+  is_a_index = sentence.find(' is a ')
+  # Make sure the format is: {Title} is a {description}
+  if is_a_index == -1:
+    return False
+
+  # Make sure it's sufficiently long
+  if wordCount < 5:
+    return False
+
+  # Only keep titles that are under a few words
+  title_length = len(sentence[:is_a_index].split())
+  if title_length > 5:
+    return False
+
+  return True
+
+def generate(num_sentences):
+  with open('description.txt') as f:
     text = f.read()
-  textModel = markovify.Text(text)
-  bestSentences = []
+  text_model = markovify.Text(text)
+  best_sentences = []
   while (True):
-    sentence = textModel.make_short_sentence(140).capitalize()
-    wordCount = len(sentence.split(" "))
-    if (sentence.find(" is a ") != -1 and wordCount > 5):
-      bestSentences.append(sentence)
-      if (len(bestSentences) == numSentences):
+    sentence = text_model.make_short_sentence(140).capitalize()
+    if is_good_sentence(sentence):
+      best_sentences.append(cleanup_sentence(sentence))
+      if (len(best_sentences) == num_sentences):
         break;
-  return bestSentences
+  return best_sentences
 
 def amazon_lambda_handler(event, context):
   return generate(min(100, event.count))
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   print generate(1)[0]
